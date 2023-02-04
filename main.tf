af568649980591501cb2ca6918a1e3e384288f33
root <root@ip-172-31-63-77.ec2.internal>
2023-02-04 14:42:17 +0000

    Added Map Variable into the main tf file

--- main.tf
+++ main.tf
@@ -7,3 +7,8 @@ resource "local_file" "devOpsfile" {
   filename = "/root/terraform/terraform-variables/file_with_vairbale.txt"
   content = "This is DevOps Test file from Variable"
 }
+
+resource "local_file" "var1" {
+ filename = "/root/terraform/terraform-variables/file_with_vairbale_map.txt"
+ content = var.content_map["content1"]
+}
--- variable.tf
+++ variable.tf
@@ -5,3 +5,10 @@ variable "filename" {
 variable "content" {
       default = "This is auto generated from a Varibale file"
 }
+variable "content_map" {
+ type = map
+ default = {
+   "content1" = "this is map1"
+   "content2" = "this is map value 2"
+}
+}
